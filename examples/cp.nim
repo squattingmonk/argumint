22860108dfd6946e6f52c94147d0281cdff3f807
Michael A. Sinclair <squattingmonk@gmail.com>
2026-07-07 18:42:09 -0500

    docs: add examples folder

---
+++ examples/cp.nim
@@ -0,0 +1,30 @@
+# This example demonstrates the cp test. The following example would fail in
+# docopt without consuming any options, no matter what you type:
+# ```
+# """cp
+# Usage:
+#  cp.py <src>... <dest>
+# """
+# ````
+#
+# Since `<src>` greedily consumes all arguments, it is never possible for
+# `<dest>` to be supplied, so parsing always fails. Since argumint supports
+# backtracking using a finite state machine, this is quite easy.
+
+
+
+import std/strformat
+
+import argumint
+
+let
+  spec = (
+    src: arg[string]("<src>", default = @[], help = "The source file(s) to copy"),
+    dest: arg("<dest>", help = "The destination to copy to"),
+    recursive: flag("-r, --recursive", help = "Whether to recurse into subdirectories"),
+    help: help()
+  )
+
+spec.parse(usage = "[-r] <src>... <dest>", prolog = "Copy files around")
+for file in spec.src:
+  echo fmt"Copying {file} to {spec.dest} (recursive: {spec.recursive})"
